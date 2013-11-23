0b845b11aa363c5d74020fcbe2be6f8a6a61b60f
dennispkramer <dennispkramer@gmail.com>
2013-11-23 13:39:26 -0500

    getting search up, defining different post types

---
+++ site/snippets/articlecontent.php
@@ -0,0 +1,8 @@
+<section class="content blogarticle">
+  <article>
+    <h2><?php echo html($page->title()) ?></h2>
+    <?php echo $page->date('Y-m-d') ?>
+    <?php echo kirbytext($page->text()) ?>
+
+  </article>
+</section>
--- site/templates/default.php
+++ site/templates/default.php
@@ -1,17 +1,62 @@
 <?php snippet('header') ?>
 
+<!-- search plugin -->
+<?php
+
+$search = new search(array(
+  'searchfield' => 'q'
+));
+
+$results = $search->results();
+
+?>
+
+<!-- search form -->
+<form action="<?php echo thisURL() ?>">
+  <input type="text" placeholder="Search…" name="q" />
+  <input type="submit" value="Search" />
+</form>
+
+<!-- search results -->
+<?php if($results): ?>
+<ul>
+  <?php foreach($results as $result): ?>
+  <li><a href="<?php echo $result->url() ?>"><?php echo $result->title() ?></a></li>
+  <?php endforeach ?>
+</ul>
+<?php endif ?>
+
 <section class="content blog">
   
   
   <?php foreach($page->children()->visible()->flip() as $article): ?>
   
   <article>
-  	<div class="post-icon"></div>
-  	<div class="post-header">
-	    <a href="<?php echo $article->url() ?>"><h2><?php echo html($article->title()) ?></h2></a>
-	    <?php echo $article->date('Y-m-d') ?>
-	</div>
-    <p><?php echo excerpt($article->text(), 300) ?></p>
+    <?php if($article->template() == 'blogarticle.bread'): ?> 
+    	<div class="post-icon post-icon-bread"></div>
+    	<div class="post-header">
+  	    <a href="<?php echo $article->url() ?>"><h2><?php echo html($article->title()) ?></h2></a>
+  	    <?php echo $article->date('Y-m-d') ?>
+      </div>
+      <p><?php echo excerpt($article->text(), 300) ?></p>
+
+    <?php elseif($article->template() == 'blogarticle.sweet'): ?> 
+      <div class="post-icon post-icon-sweet"></div>
+      <div class="post-header">
+        <a href="<?php echo $article->url() ?>"><h2><?php echo html($article->title()) ?></h2></a>
+        <?php echo $article->date('Y-m-d') ?>
+      </div>
+      <p><?php echo excerpt($article->text(), 300) ?></p>
+
+    <?php elseif($article->template() == 'blogarticle.savory'): ?> 
+      <div class="post-icon post-icon-savory"></div>
+      <div class="post-header">
+        <a href="<?php echo $article->url() ?>"><h2><?php echo html($article->title()) ?></h2></a>
+        <?php echo $article->date('Y-m-d') ?>
+      </div>
+      <p><?php echo excerpt($article->text(), 300) ?></p>
+
+      <?php endif ?> 
   </article>
 
   <?php endforeach ?>
