5258cca7b256fd274e1bae51fd2c3e9fda552e42
dennispkramer <dennispkramer@gmail.com>
2013-12-06 08:21:01 -0500

    image blurs on scroll

--- assets/js/control.js
+++ assets/js/control.js
@@ -1,4 +1,6 @@
 $(window).scroll(function(){
 var opac = $(window).scrollTop() / ($('.header-image').height() - 50);
+var blur = $(window).scrollTop() / 30;
 $('.header-image').css({'background': 'linear-gradient(rgba(0,0,0,' + opac + '),rgba(0,0,0,' + opac + ')), url(/assets/images/01.jpg)', 'background-repeat':'no-repeat','background-size':'cover', 'background-position': 'center'});
+$('.header-image').css('-webkit-filter', 'blur(0'+ blur + 'px)');
 });
