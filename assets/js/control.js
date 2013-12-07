b4f988a6ec24ca261beec81009b9de7cc5f3bc34
dennispkramer <dennispkramer@gmail.com>
2013-12-07 11:50:04 -0500

    fixing opacity on image scroll

--- assets/js/control.js
+++ assets/js/control.js
@@ -1,5 +1,5 @@
 $(window).scroll(function(){
-	var opac = $(window).scrollTop() / ($('.header-image').height() - 500);
+	var opac = $(window).scrollTop() / ($('.header-image').height() + 400);
 	var blur = $(window).scrollTop() / 30;
 	$('.header-image').css({'background': 'linear-gradient(rgba(0,0,0,' + opac + '),rgba(0,0,0,' + opac + ')), url(/assets/images/01.jpg)', 'background-repeat':'no-repeat','background-size':'cover', 'background-position': 'center'});
 	$('.header-image').css('-webkit-filter', 'blur('+ blur + 'px)');
