5a0a0d1c88b8a05118d20c24c851a42f435510a3
dennispkramer <dennispkramer@gmail.com>
2013-12-06 22:06:10 -0500

    fixing up header

--- assets/js/control.js
+++ assets/js/control.js
@@ -1,5 +1,5 @@
 $(window).scroll(function(){
-	var opac = $(window).scrollTop() / ($('.header-image').height() - 50);
+	var opac = $(window).scrollTop() / ($('.header-image').height() - 500);
 	var blur = $(window).scrollTop() / 30;
 	$('.header-image').css({'background': 'linear-gradient(rgba(0,0,0,' + opac + '),rgba(0,0,0,' + opac + ')), url(/assets/images/01.jpg)', 'background-repeat':'no-repeat','background-size':'cover', 'background-position': 'center'});
 	$('.header-image').css('-webkit-filter', 'blur('+ blur + 'px)');
@@ -7,7 +7,7 @@ $(window).scroll(function(){
 
 
 $(window).scroll(function(){
-    if ($(this).scrollTop() > 500) {
+    if ($(this).scrollTop() > 400) {
         $('.header-image').css('-webkit-filter', 'blur(0)');
     }
     else {
