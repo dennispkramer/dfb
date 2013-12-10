253759f253ca538d455031ff66b4f6b68b31a174
dennispkramer <dennispkramer@gmail.com>
2013-12-09 19:43:27 -0500

    fixing header image transition on articles

--- assets/js/control.js
+++ assets/js/control.js
@@ -1,7 +1,7 @@
 $(window).scroll(function(){
-	var opac = $(window).scrollTop() / ($('.header-image').height() + 400);
+	var opac = $(window).scrollTop() / ($('.image-wrap').height() - 50);
 	var blur = $(window).scrollTop() / 30;
-	$('.header-image').css({'background': 'linear-gradient(rgba(0,0,0,' + opac + '),rgba(0,0,0,' + opac + ')), url(/assets/images/01.jpg)', 'background-repeat':'no-repeat','background-size':'cover', 'background-position': 'center'});
+	$('.image-opacity-fade').css('background', 'rgba(0,0,0,' + opac + ')');
 	$('.header-image').css('-webkit-filter', 'blur('+ blur + 'px)');
 });
 
--- site/snippets/articlecontent.php
+++ site/snippets/articlecontent.php
@@ -1,5 +1,7 @@
 <?php if($page->hasImages()): ?>
 	<div class="image-wrap"><img class="header-image" src="<?php echo $page->images()->first()->url(); ?>" /></div>
+	<div class="image-opacity-fade"</div>
+	<div class="image-opacity"</div>
 	<h1 class="page-title"><?php echo html($page->title()) ?></h1>
 <?php endif ?>
 
