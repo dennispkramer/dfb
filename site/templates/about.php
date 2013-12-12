2732087a2c5ea422d829bb50b5a7e7ff7068d704
dennispkramer <dennispkramer@gmail.com>
2013-12-11 21:08:31 -0500

    messing with the type stack

--- site/templates/about.php
+++ site/templates/about.php
@@ -1,14 +1,13 @@
 <?php snippet('header') ?>
 
-<div class="header-image"></div>
-<h1 class="page-title"><?php echo html($page->title()) ?></h1>
-
-<div class="false-background">
-	<section class="content">
-
-		<h2><?php echo html($page->subtitle()) ?></h2>
-		<?php echo kirbytext($page->text()) ?>
-	</section>
+<div class="default-header">
+  <h1 class="page-title"><?php echo html($page->title()) ?></h1>
 </div>
+
+<section class="content">
+
+	<h2><?php echo html($page->subtitle()) ?></h2>
+	<?php echo kirbytext($page->text()) ?>
+</section>
 
 <?php snippet('footer') ?>
