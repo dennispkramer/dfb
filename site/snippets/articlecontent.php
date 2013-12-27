42fdccff1afa836f4bd11f4dd994ac726c409be7
dennispkramer <dennispkramer@gmail.com>
2013-12-27 12:07:39 -0500

    cleaning up article image header

--- site/snippets/articlecontent.php
+++ site/snippets/articlecontent.php
@@ -5,17 +5,19 @@
 	<div class="title-wrap">
 		<h1 class="page-title"><?php echo html($page->title()) ?></h1>
 		<h2 class="page-subheader"><?php echo html($page->subheader()) ?></h2>
-		<!-- bread posts -->
-		<?php if($page->template() == 'blogarticle.bread'): ?> 
-			<div class="post-icon post-icon-detail post-icon-bread"></div>
-		<!-- sweet posts -->
-		<?php elseif($page->template() == 'blogarticle.sweet'): ?> 
-			<div class="post-icon post-icon-detail post-icon-sweet"></div>
-		<!-- savory posts -->
-		<?php elseif($page->template() == 'blogarticle.savory'): ?> 
-			<div class="post-icon post-icon-detail post-icon-savory"></div>
-		<?php endif ?> 
-		<div class="post-date post-date-detail">Written on <?php echo $page->date('m-d-y') ?></div>
+		<div class="page-meta">
+			<!-- bread posts -->
+			<?php if($page->template() == 'blogarticle.bread'): ?> 
+				<div class="post-icon post-icon-detail post-icon-bread"></div>
+			<!-- sweet posts -->
+			<?php elseif($page->template() == 'blogarticle.sweet'): ?> 
+				<div class="post-icon post-icon-detail post-icon-sweet"></div>
+			<!-- savory posts -->
+			<?php elseif($page->template() == 'blogarticle.savory'): ?> 
+				<div class="post-icon post-icon-detail post-icon-savory"></div>
+			<?php endif ?> 
+			<div class="post-date post-date-detail">Written on <?php echo $page->date('m-d-y') ?></div>
+		</div>
 	</div>
 
 <?php endif ?>
